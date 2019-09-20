01eab758cd5f5a9fa51801e03ca342cf055795e3
free020603 <34528924+free020603@users.noreply.github.com>
2019-09-20 11:09:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dennadiner/Food.java
@@ -0,0 +1,14 @@
+package com.example.dennadiner;
+
+public class Food
+{
+    private String foodName;
+    private String description;
+    private double price;
+    private int imageResourceID;
+
+    public Food(String foodName, String description, double price, int imageResourceID);
+        this.foodName = foodName;
+        this.description = description;
+        this.price
+}
